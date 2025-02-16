d4cd0f7dbceef8ea7d8af02004369dcf9b408165
Kathirvel E <eshakathirvel@gmail.com>
2025-02-16 11:29:42 +0530

    Add files via upload

---
+++ DAY-4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int b;
+printf("enter a number:");
+scanf("%d",&b);
+if(b%2==0){
+printf("enterd number is even number");
+}
+else{
+printf("enterd number is odd number");
+}
+return 0;
+}
